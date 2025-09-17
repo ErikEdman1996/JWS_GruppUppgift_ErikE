8a4c0086fcbc0a6c269704d0666c017f4f99d160
Erik Edman <61149651+ErikEdman1996@users.noreply.github.com>
2025-09-17 11:59:40 +0200

    5th commit

---
+++ src/main/java/org/example/jws_gruppuppgift/entities/Booking.java
@@ -0,0 +1,129 @@
+package org.example.jws_gruppuppgift.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Booking
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String customer;
+
+    @Column(name = "departure")
+    private LocalDate departureDate;
+
+    @Column
+    private int weeks;
+
+    @Column
+    private String hotel;
+
+    @Column(name = "destination_city")
+    private String city;
+
+    @Column(name = "destination_country")
+    private String country;
+
+    @Column
+    private float totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "travel_id")
+    private Travel travel;
+
+    public Booking()
+    {
+
+    }
+
+    public Booking(Long id, String customer, LocalDate departureDate, int weeks, Travel travel)
+    {
+        this.id = id;
+        this.customer = customer;
+        this.departureDate = departureDate;
+        this.weeks = weeks;
+        this.travel = travel;
+
+        this.hotel = travel.getHotel();
+        this.city = travel.getDestination().getCity();
+        this.country = travel.getDestination().getCountry();
+        this.totalPrice = travel.getPricePerWeek() * weeks;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public int getWeeks() {
+        return weeks;
+    }
+
+    public void setWeeks(int weeks) {
+        this.weeks = weeks;
+    }
+
+    public String getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(String hotel) {
+        this.hotel = hotel;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Travel getTravel() {
+        return travel;
+    }
+
+    public void setTravel(Travel travel) {
+        this.travel = travel;
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,7 @@ INSERT INTO destination(city, country) VALUES ("Tokyo", "Japan");
 insert into travel(price, hotel, destination_id) values (1000, "The Waldorf Hilton", 2);
 insert into travel(price, hotel, destination_id) values (1200, "The Plaza", 4);
 insert into travel(price, hotel, destination_id) values (1500, "Dai-ichi Hotel", 5);
+
+INSERT INTO booking(customer, departure, weeks, hotel, destination_city, destination_country, total_price, travel_id)
+VALUES("John Doe", "2025-10-05", 2, "The Plaza", "New York", "United States",
+       1200 * 2, 2);
