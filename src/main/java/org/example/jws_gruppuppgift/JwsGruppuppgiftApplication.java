1bbf77da9f3965e9d9fd7be1c4995663ae261f51
Erik Edman <61149651+ErikEdman1996@users.noreply.github.com>
2025-09-17 10:25:59 +0200

    First commit

---
+++ src/main/java/org/example/jws_gruppuppgift/JwsGruppuppgiftApplication.java
@@ -0,0 +1,13 @@
+package org.example.jws_gruppuppgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwsGruppuppgiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwsGruppuppgiftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jws_gruppuppgift/JwsGruppuppgiftApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jws_gruppuppgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwsGruppuppgiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
